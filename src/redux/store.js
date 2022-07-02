1d893bcb90c1ff82640380e8f9c5500ab37d8f4a
ValeriiaMalyshko <91088527+ValeriiaMalyshko@users.noreply.github.com>
2022-07-02 21:28:56 +0300

    store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
-import { createStore, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from '@reduxjs/toolkit';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import contactsReducer from './contacts-reducer';
 
-const rootReducer = combineReducers({
-  todos: contactsReducer,
+const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+  },
 });
-
-const store = createStore(rootReducer, composeWithDevTools());
+// const rootReducer = combineReducers({});
+// export type RootState = ReturnType<typeof rootReducer>;
+// export type RootState = ReturnType<typeof store.getState>;
 
 export default store;
